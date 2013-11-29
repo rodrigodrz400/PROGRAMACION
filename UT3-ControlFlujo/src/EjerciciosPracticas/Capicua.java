db88fd5fdd69ee4b407e0981fb023b458be32389
Rodrigo Fdez. <azarquiel.372775@gmail.com>
2013-11-29 13:32:28 +0100

    29/11/2013

--- UT3-ControlFlujo/src/EjerciciosPracticas/Capicua.java
+++ UT3-ControlFlujo/src/EjerciciosPracticas/Capicua.java
@@ -17,10 +17,12 @@ public class Capicua {
 	//1.Para obtener el último dígito del número lo haremos con el resto de una división entre diez. 
     //2.Del número original hay que quitarle el último dígito. En este caso utilizamos la división entre 10.
 	
-	long numeroRestante,numeroInvertido,resto;
+	long numeroRestante;
+	long numeroInvertido;
+	long resto;
 
 	while(numero<=0);
-		numeroRestante=numero;
+		 numeroRestante=numero;
 		 numeroInvertido=0;
 		 resto=0;
 
@@ -34,9 +36,9 @@ public class Capicua {
 	
 	// Si el número origen y el destino son el mismo, es que es capicua
 		if(numeroInvertido==numero)
-		 System.out.println("El numero es capicua\n");
+		 System.out.println("El número " + numeroInvertido + ", SÍ es capicua.");
 			else
-			 System.out.println("El numero no es capicua\n");
+			 System.out.println("El número " + numero + ", NO es capicua.");
 	
 	teclado.close();
 				
