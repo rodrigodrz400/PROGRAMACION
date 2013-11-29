d845a0fe22a3f2d7923ba88fb3069984f7ec0cc2
Rodrigo Fdez. <azarquiel.372775@gmail.com>
2013-11-29 13:24:51 +0100

    29/11/2013

---
+++ UT3-ControlFlujo/src/EjerciciosPracticas/Capicua.java
@@ -0,0 +1,48 @@
+package EjerciciosPracticas;
+
+import java.util.Scanner;
+
+public class Capicua {
+
+	//Lee un número por consola y comprueba si es palíndromo (capicúa).
+	//Pista: si creas un número con los dígitos en orden inverso al leido, 
+	//y al compararlos resulta que son iguales, será capicúa.
+	public static void main(String[] args) {
+	long numero= 0;
+	
+	System.out.println("Inserte un número para comprobar si es o no capicúa");
+	Scanner teclado = new Scanner (System.in);
+	numero = teclado.nextLong();
+	
+	//1.Para obtener el último dígito del número lo haremos con el resto de una división entre diez. 
+    //2.Del número original hay que quitarle el último dígito. En este caso utilizamos la división entre 10.
+	
+	long numeroRestante,numeroInvertido,resto;
+
+	while(numero<=0);
+		numeroRestante=numero;
+		 numeroInvertido=0;
+		 resto=0;
+
+	// El código que viene a continuación da la vuelta a un numero
+	while(numeroRestante!=0)
+		{
+		 resto=numeroRestante%10;
+		 numeroInvertido=numeroInvertido*10+resto;
+		 numeroRestante=numeroRestante/10;		 		 
+		}
+	
+	// Si el número origen y el destino son el mismo, es que es capicua
+		if(numeroInvertido==numero)
+		 System.out.println("El numero es capicua\n");
+			else
+			 System.out.println("El numero no es capicua\n");
+	
+	teclado.close();
+				
+}
+
+}
+
+		
+		
